f86a04309d9cd21fde2ef946cb49abf6487d536b
Avitej <82682098+Avitej@users.noreply.github.com>
2022-10-30 17:22:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -8,19 +8,25 @@ function preload() {
 function setup() {
     canvas = createCanvas(620, 420);
     canvas.center();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detection Objects";
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
 }
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 400, 400);
     if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
         for (i = 0; i < objects.length; i++){
             document.getElementById("status").innerHTML = "Status : Objects Detected";
-            fill("#FF0000");
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+            fill(r, g, b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + "" + percent + "%" + objects[i].x, objects[i].y);
             noFill();
-            stroke("#FF0000");
+            stroke(r, g, b);
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     }
@@ -28,7 +34,7 @@ function draw() {
 function modelLoaded (){
     console.log("model loaded");
     status= true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(video, gotResult);
 }
 
 function gotResult (error, results) {
@@ -37,4 +43,9 @@ function gotResult (error, results) {
     }
     console.log(results);
     objects = results;
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd, modelLoaded');
+    document.getElementById("status").innerHTML = "status : detecting objects";
 }
